06e3b6f4172725176374b6f6117ae1f94545b7ce
HeLLLuci <mustkeemmbaraskar@coep.sveri.ac.in>
2023-09-11 19:20:42 +0530

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color orangeColor = Colors.deepOrange;
+const Color whiteColor = Colors.white;
+const Color greenColor = Colors.green;
+const Color blackColor = Colors.black;
+
+const headingTextStyle = TextStyle(fontSize: 30, color:greenColor, fontWeight: FontWeight.w500 );
+const headTextStyle = TextStyle(color: Colors.white);
---
+++ lib/evaluator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Eva extends StatefulWidget {
+  const Eva({super.key});
+
+  @override
+  State<Eva> createState() => _EvaState();
+}
+
+class _EvaState extends State<Eva> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
